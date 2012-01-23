965159bd342050e0337adbded57f07646b9fcccc
Rob Baker <rrbaker@gmail.com>
2012-01-23 07:11:56 -0500

    bugs in header border, extra char in template

--- one-col.tpl.php
+++ one-col.tpl.php
@@ -1,4 +1,4 @@
-x<?php
+<?php
 // $Id: page.tpl.php 7156 2010-04-24 16:48:35Z chris $
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>">
